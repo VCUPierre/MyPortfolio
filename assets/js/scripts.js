4a3189ba0a812b13da69caf4d4d9f6a2b68f8a48
Pierre R <randalltp@mymail.vcu.edu>
2019-10-08 10:18:26 -0400

    Added basic functionality to populate repos on porfolio view

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $("#home").removeClass("d-none");
+    getGithubRepos();
 })
 $(document).on("click", "#btnAbout", function(event){
     event.preventDefault();
@@ -16,17 +17,43 @@ $(document).on("click", ".logo", function(){
     $("#portfolio").addClass("d-none");
     $("#home").removeClass("d-none");
 })
-function getGitHubRepos(){
+function getGithubRepos(){
     let gitArray = ['GifTastic', 'TriviaGame','Responsive-Portfolio','WordGuessGame','DeliveryBusinessWebConcept', 'localRestaurantWebConcept'];
     $.ajax({
         url: 'https://api.github.com/users/vcuPierre/repos?type=owner&sort=created',
         method: "GET"
     }).done(function(data){
         console.log(data);
+        var dataArray = [];
         for (let i = 0; i < data.length; i++){
             if (gitArray.includes(data[i].name)){
-                console.log(i);
+                dataArray.push(data[i]);
             }
         }
+        addGithubRepos(dataArray);
     });
+}
+function addGithubRepos(results){
+    var htmlHolder =  $("#projectHolder");
+
+    for (let i = 0; i < results.length; i++){
+        let mainDiv = $("<div>", {id: "project"+i});
+        let rowDiv = $("<div>", {class: "row"});
+        let leftDiv = $("<div>", {class: "col-8 text-center"});
+        let rightDiv = $("<div>", {class: "col-4 text-center"});
+        let projectTitleDiv = $("<div>", {class: "title"});
+        let projectDescDiv = $("<div>", {class: "description"});
+        let projectLangDiv = $("<div>", {class: "lang"});
+        let projectButton = $("<a>", {href: results[i].html_url, class: "btn btn-outline-success"});
+        let projectImg = $("<img>", {src: "assets/imgs&Files/IMG_1708.JPG", alt: " ", class: "logo"});
+        projectTitleDiv.append($("<h3>").text(results[i].name));
+        projectDescDiv.append($("<h4>").text(results[i].description));
+        projectLangDiv.append($("<h5>").text(results[i].language));
+        projectButton.text("Git Repo");
+        leftDiv.append(projectTitleDiv, projectDescDiv, projectLangDiv, projectButton);
+        rightDiv.append(projectImg);
+        rowDiv.append(leftDiv, rightDiv);
+        mainDiv.append(rowDiv, $("<hr>"));
+        mainDiv.appendTo(htmlHolder);
+    }
 }
